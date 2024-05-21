6af35f9f27b902c481f2d3f4c8a45b75d3fc63e2
oussama-benyamina <146718357+oussama-benyamina@users.noreply.github.com>
2024-05-21 16:41:13 +0200

    fin du project

---
+++ jour01/job03/index.php
@@ -0,0 +1,41 @@
+<?php
+// Variables de types primitifs
+$booleanVar = true;
+$intVar = 42;
+$stringVar = "Hello LaPlateforme";
+$floatVar = 3.14159;
+
+
+echo "
+<table border='1'>
+    <thaed>
+        <tr>
+        <th>type </>
+        <th>name </>
+        <th>value</>
+        
+        </tr>
+    </thaed>    
+    <tbody>
+    <tr>
+        <td>boolean</td>    
+        <td>booleanVar</td>
+        <td>$booleanVar</td>
+    </tr>
+    <tr>
+        <td>int</td>
+        <td>intVar</td>
+        <td>$intVar</td>
+    </tr>
+    <tr>
+        <td>string</td>
+        <td>stringVar</td>
+        <td>$stringVar</td>
+    </tr>
+    <tr>
+        <td>float</td>
+        <td>floatVar</td>
+        <td>$floatVar</td>
+    </tr>
+    </tbody>
+";?>
